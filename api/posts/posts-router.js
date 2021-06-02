3e7826775c0ba9571124cc59b248df36ae55f266
MichaelMcFaddenjr <michaelpmcfaddenjr@gmail.com>
2021-06-01 19:04:20 -0600

    wip

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -15,7 +15,7 @@ router.get('/', (req, res) => {
         message: 'The posts information could not be retrieved',
       })
     })
-})
+});
 
 router.get('/:id', async (req, res) => {
   try {
@@ -33,12 +33,12 @@ router.get('/:id', async (req, res) => {
       message: 'The post information could not be retrieved',
     })
   }
-})
+});
 
 router.post('/', (req, res) => {
   const { title, contents } = req.body;
   if (!title || !contents ) {
-    req.status(400).json({
+    res.status(400).json({
       message:'Please provide title and contents for the post'
     })
   } else {
@@ -55,6 +55,62 @@ router.post('/', (req, res) => {
           message: 'There was an error while saving the post to the database'
         })
       })
+  }
+});
+
+router.put('/:id', (req, res) => {
+  const { title, contents } = req.body;
+  if (!title || !contents ) {
+    res.status(400).json({
+      message:'Please provide title and contents for the post'
+    })
+  } else {
+    Posts.findById(req.params.id)
+    .then(post => {
+      if (!post) {
+        res.status(404).json({
+          message: 'The post with the specified ID does not exist'
+        })
+      } else {
+        return Posts.update(req.params.id, req.body)
+      }
+    })
+    .then(data => {
+      if (data) {
+        return Posts.findById(req.params.id)
+      }
+    })
+    .then(post => {
+      res.json(post)
+    })
+    .catch (err => {
+      res.status(500).json({
+        message:'The post with the specified ID does not exist',
+        err: err.message,
+        stack: err.stack,
+      })
+    }
+    )
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try{
+    const post = await Posts.findById(req.params.id)
+    if (!post) {
+      res.status(404).json({
+        message:'The post with the specified ID does not exist',
+      })
+    } else {
+      await Posts.remove(req.params.id)
+      res.json(post)
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: 'The post could not be removed',
+      error: error.message,
+      stack: error.stack,
+    })
   }
 });
 
